dcb6d4b49292736fa8f8d0d51d767e7c4e05c6ee
Krzysztof Druciarek <kkd26@cam.ac.uk>
2021-10-08 08:03:10 +0100

    Lesson 8 - varaible types and names

---
+++ kdruc/lesson8/Test.java
@@ -0,0 +1,22 @@
+package kdruc.lesson8;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Test {
+
+  String name;
+  int age;
+  float power;
+
+  List<Integer> my_list;
+  List<String> second_list;
+
+
+  void fun() {
+    List<Integer> my_unique_list_name = new ArrayList<>();
+    my_list = new LinkedList<>();
+    second_list = null;
+  }
+}
