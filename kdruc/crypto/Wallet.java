681ab5581ac1c8dff342662303fc7d6e2b303676
Krzysztof Druciarek <kkd26@cam.ac.uk>
2022-02-14 10:15:41 +0000

    Lesson 25 - intro to BlockChain, create wallet with private/public key pair

---
+++ kdruc/crypto/Wallet.java
@@ -0,0 +1,25 @@
+package kdruc.crypto;
+
+import java.security.*;
+
+public class Wallet {
+  private PublicKey publicKey = null; // your identity
+  private PrivateKey privateKey = null; // your password
+
+  KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+
+  public Wallet() throws NoSuchAlgorithmException {
+    generateKeys();
+  }
+
+  private void generateKeys() {
+    keyGen.initialize(2048);
+    KeyPair keyPair = keyGen.generateKeyPair();
+    publicKey = keyPair.getPublic();
+    privateKey = keyPair.getPrivate();
+  }
+
+  public String getKey() {
+    return publicKey.toString();
+  }
+}
