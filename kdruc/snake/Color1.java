5217da560c74bb2ee3b9bbfc2d6ba6f4aedf217b
piotr bielinski <piotrbielinski@MacBook-Pro-piotr.local>
2022-05-12 15:03:39 +0200

    Testing color enum -wip

---
+++ kdruc/snake/Color1.java
@@ -0,0 +1,16 @@
+package kdruc.snake;
+
+import java.awt.*;
+
+public enum Color1 {
+		White(Color.WHITE),
+		Black(Color.BLACK),
+		Hmm(new Color(30,30,50));
+
+		private Color color;
+
+		Color1(Color color) {
+				this.color = color;
+
+		}
+}
