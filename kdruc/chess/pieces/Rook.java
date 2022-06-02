f7326438ae0ed9b5b8b68a202f0020cbdef9daf0
piotr bielinski <piotrbielinski@MacBook-Pro-piotr.local>
2022-06-02 14:33:57 +0200

    Add rook movement

--- kdruc/chess/pieces/Rook.java
+++ kdruc/chess/pieces/Rook.java
@@ -4,7 +4,10 @@ import kdruc.chess.Board;
 import kdruc.chess.pieceattributes.PieceColor;
 import kdruc.chess.pieceattributes.Position;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static kdruc.chess.pieceattributes.Position.getPosition;
 
 public class Rook extends Piece {
 	public Rook(PieceColor pieceColor, Position position) {
@@ -19,7 +22,17 @@ public class Rook extends Piece {
 
 	@Override
 	public List<Position> getAllPositions(Board board) {
-		return null;
+		List<Position> positions = new ArrayList<>();
+		int column = this.position().column();
+		int row = this.position().row();
+		for (int i = 0; i < Board.SIZE; i++) {
+			Position target1 = getPosition(column, i);
+			Position target2 = getPosition(i, row);
+			positions.add(target1);
+			positions.add(target2);
+		}
+
+		return positions;
 	}
 
 }
